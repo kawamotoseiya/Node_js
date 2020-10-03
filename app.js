99f29f366eebb7947cd71d307f6f7bfe9532b785
vagrant <seinnto1226@icloud.com>
2020-10-03 14:43:26 +0000

    mysql

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const express = require('express');
+require('dotenv').config();
+const env = process.env;
+const mysql = require('mysql');
 const app = express();
 
 app.use(express.static('public'));
+
+
+const connection = mysql.createConnection({
+  host: env.HOST,
+  user: env.USER,
+  password: env.PASSWORD,
+  database: env.DATABASE
+});
 
 
 
 app.get('/', (req, res) => {
   res.render('top.ejs');
 });
+
 app.get('/index',(req, res) => {
-	res.render("index.ejs");
+	connection.query(
+		'SELECT * FROM items',
+		(error, results) => {
+			res.render('index.ejs',{items: results});
+		});
 });
 
 app.listen(3000);
